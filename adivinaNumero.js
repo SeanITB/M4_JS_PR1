ed8ca2b41e97c1d8aed72e14749c5be1617d25e3
sjo <sean.costelloe.7e5@itb.cat>
2023-11-07 13:57:28 +0100

    Afergir articles recorrer les posicions de numero i numero usuari

--- adivinaNumero.js
+++ adivinaNumero.js
@@ -1,31 +1,40 @@
 function comprobasj() {
-    //vars
-    var aleatoi = Math.ceil(Math.random()*10000)
+    // Obte els numeros aleatoris
+    var aleatori = Math.ceil(Math.random()*10000)
 
-    //lets
-    let numeroUsuari;
-    let intents;
-    
-    //Obtindre els numeros
-    numeroUsuari = document.getElementById("contingutComproba").value;
+    //Obtindre el numero del usuari
+    let numeroUsuari = document.getElementById("contingutComproba").value;
+
+    // Passar els numeros a strings
+    let strAleatori = aleatori.toString()
+    let strNumeroUsuari = numeroUsuari.toString()
+
+
+    // Mostra el numero d'usuari per la consola
     console.log(numeroUsuari);
-
-    do {
-        intents = 0;
-        intents++;
-        let incorrecte = (numeroUsuari > aleatoi)?true:false;
-        if (incorrecte == true){
-            document.getElementById("text").innerHTML = "No és correcte, introdueix un numero MÉS GRAN que: " + numeroUsuari + ". Portes " + intents + " intents.";
-            console.log("obtindre 1r num " +  (numeroUsuari%10));
-        } 
-        else if (incorrecte == false) {
-            document.getElementById("text").innerHTML = "No és correcte, introdueix un numero MÉS PETIT que: " + numeroUsuari + ". Portes " + intents + " intents.";
-            console.log("obtindre 1r num " + (numeroUsuari%10));
-        }
-    }while((aleatoi != numeroUsuari))
-    //Mostrar la sortida
-    if (aleatoi == numeroUsuari) {
-        document.getElementsByClassName("digito").innerHTML = aleatori;
-    } 
     
+    
+    //Comprobar si el numero és correcte
+    if (aleatori == numeroUsuari){
+        for (digit = 0; digit < 5; digit++) {
+            document.getElementsByClassName("digitNumeroAleatori")[digit].innerHTML = strAleatori[digit]
+        } 
+    } 
+    else {
+        //Mostrar el misatge d'error
+        document.getElementById("text").innerHTML = "No és correcte, torna a intentar.";
+
+        // Crear articles en resultat
+        for (cubito = 0; cubito < 5; cubito++) {
+            let resultat = document.getElementById("resultat");
+            let creaArticle = document.createElement("article");
+            resultat.appendChild(creaArticle);
+        }
+
+        // Posar el numero d'usuari
+        for (digit = 0; digit < 5; digit++) {
+            //document.getElementsByClassName("digitResultat")[digit].innerHTML = strNumeroUsuari[digit]
+        }   
+    }   
 }
+
