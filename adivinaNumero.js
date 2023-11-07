4ab7b019686f5d1a85d150e4fec9b02272bade3e
SeanITB <146827341+SeanITB@users.noreply.github.com>
2023-11-07 11:27:27 +0100

    Update adivinaNumero.js
    canvis

--- adivinaNumero.js
+++ adivinaNumero.js
@@ -3,38 +3,29 @@ function comprobasj() {
     var aleatoi = Math.ceil(Math.random()*10000)
 
     //lets
-    let element;
     let numeroUsuari;
-    let sortidaPrograma;
     let intents;
     
     //Obtindre els numeros
-    element = document.getElementById("contingutComproba");
-    numeroUsuari = element.value;
+    numeroUsuari = document.getElementById("contingutComproba").value;
     console.log(numeroUsuari);
-    //mentres el numero que ha introduit l'usuari sigui diferent al numero de la sort, demana el numero.
-    /*numeroUsuari = window.prompt("Intenta hacertar un numero enre 0 a 100");*/
+
     do {
         intents = 0;
         intents++;
         let incorrecte = (numeroUsuari > aleatoi)?true:false;
-        console.log(intents);
         if (incorrecte == true){
-            document.getElementsByTagName("p")[0].innerHTML = "No és correcte, introdueix un numero MÉS GRAN que: " + numeroUsuari + ". Portes " + intents + " intents.";
-            /*numeroUsuari = window.prompt("El numero " + numeroUsuari + " es mes gran que el numero aleatori. Introdueix un altre cop el numero, pero que sigui mes petit que: " + numeroUsuariAnterior)*/
+            document.getElementById("text").innerHTML = "No és correcte, introdueix un numero MÉS GRAN que: " + numeroUsuari + ". Portes " + intents + " intents.";
+            console.log("obtindre 1r num " +  (numeroUsuari%10));
         } 
         else if (incorrecte == false) {
-            document.getElementsByTagName("p")[0].innerHTML = "No és correcte, introdueix un numero MÉS PETIT que: " + numeroUsuari + ". Portes " + intents + " intents.";
-            /*numeroUsuari = window.prompt("El numero " + numeroUsuari + " es mes petit que el numero aleatori. Introdueix un altre cop el numero, pero que sigui mes gran que: " + numeroUsuariAnterior)*/
+            document.getElementById("text").innerHTML = "No és correcte, introdueix un numero MÉS PETIT que: " + numeroUsuari + ". Portes " + intents + " intents.";
+            console.log("obtindre 1r num " + (numeroUsuari%10));
         }
-        //sortidaPrograma = window.confirm("Bols seguir en el programa?")
-    }while((aleatoi != numeroUsuari) && (sortidaPrograma))
+    }while((aleatoi != numeroUsuari))
     //Mostrar la sortida
     if (aleatoi == numeroUsuari) {
-        //window.alert("L'has trobat");
         document.getElementsByClassName("digito").innerHTML = aleatori;
     } 
-    else {
-        //window.alert("Has tencat el programa")
-    }
+    
 }
