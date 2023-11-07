f79452641fde41451bb937b4e759f91d6db8ff69
SeanITB <146827341+SeanITB@users.noreply.github.com>
2023-11-07 16:19:17 +0100

    Update adivinaNumero.js
    toDo class

--- adivinaNumero.js
+++ adivinaNumero.js
@@ -1,7 +1,7 @@
 function comprobasj() {
     // Obte els numeros aleatoris
-    var aleatori = Math.ceil(Math.random()*10000)
-
+    var aleatori = Math.ceil(Math.random()*100000)
+    console.log(aleatori)
     //Obtindre el numero del usuari
     let numeroUsuari = document.getElementById("contingutComproba").value;
 
@@ -9,6 +9,11 @@ function comprobasj() {
     let strAleatori = aleatori.toString()
     let strNumeroUsuari = numeroUsuari.toString()
 
+    // Pasar numero aleatori a un array
+    var arrayAleatori = String(aleatori).split("").map((aleatori)=>{
+        return Number(aleatori)
+      })
+    //console.log(arrayAleatori)
 
     // Mostra el numero d'usuari per la consola
     console.log(numeroUsuari);
@@ -28,13 +33,26 @@ function comprobasj() {
         for (cubito = 0; cubito < 5; cubito++) {
             let resultat = document.getElementById("resultat");
             let creaArticle = document.createElement("article");
-            resultat.appendChild(creaArticle);
+            let article = resultat.appendChild(creaArticle);
+            article.classList.add("digitResultat");
         }
 
         // Posar el numero d'usuari
-        for (digit = 0; digit < 5; digit++) {
-            //document.getElementsByClassName("digitResultat")[digit].innerHTML = strNumeroUsuari[digit]
+        let posicioIncial = 0
+        for (digit = posicioIncial; digit < 5; digit++) {
+            document.getElementsByClassName("digitResultat")[digit].innerHTML = strNumeroUsuari[digit];
+            let intAleatori = strAleatori.toInt();
+            let intNumeroUsuari = strNumeroUsuari.toInt()[digit];
+            if (intAleatori == intNumeroUsuari) {
+                document.getElementsByClassName("digitResultat").style.property = "background-color: green";
+            }
+            else if (arrayAleatori.includes(intAleatori)) {
+                document.getElementsByClassName("digitResultat").style.property = "background-color: yellow";
+            }
+            
+
         }   
+        posicioIncial += 5
     }   
 }
 
